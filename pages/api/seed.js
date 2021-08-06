ac5e8ce5c5d5c9d1e31e2891f03cc6ffdc4d54e7
sleekcharly <c_ukasoanya@yahoo.com>
2021-08-06 14:05:29 +0100

    seed test users to database

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -2,11 +2,16 @@ import nc from "next-connect";
 import Product from "../../models/Product";
 import db from "../../utils/db";
 import data from "../../utils/data";
+import User from "../../models/user";
 
 const handler = nc();
 
 handler.get(async (req, res) => {
   await db.connect();
+  // delete all users in User model
+  await User.deleteMany();
+  // insert users from data
+  await User.insertMany(data.products);
   //   delete all products i Product model
   await Product.deleteMany();
   //   insert products from data
