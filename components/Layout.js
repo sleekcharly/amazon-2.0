bbbbda397812b2f60fb4549f8ee548a258ef0557
sleekcharly <c_ukasoanya@yahoo.com>
2021-08-09 14:41:06 +0100

    created ordes history page and api

--- components/Layout.js
+++ components/Layout.js
@@ -63,8 +63,11 @@ export default function Layout({ title, description, children }) {
   const loginClickhandler = e => {
     setAnchorEl(e.currentTarget);
   };
-  const loginMenuCloseHandler = () => {
+  const loginMenuCloseHandler = (e, redirect) => {
     setAnchorEl(null);
+    if (redirect) {
+      router.push(redirect);
+    }
   };
   const logoutClickHandler = () => {
     setAnchorEl(null);
@@ -130,9 +133,15 @@ export default function Layout({ title, description, children }) {
                     open={Boolean(anchorEl)}
                     onClose={loginMenuCloseHandler}
                   >
-                    <MenuItem onClick={loginMenuCloseHandler}>Profile</MenuItem>
-                    <MenuItem onClick={loginMenuCloseHandler}>
-                      My account
+                    <MenuItem
+                      onClick={e => loginMenuCloseHandler(e, "/profile")}
+                    >
+                      Profile
+                    </MenuItem>
+                    <MenuItem
+                      onClick={e => loginMenuCloseHandler(e, "/order-history")}
+                    >
+                      Order History
                     </MenuItem>
                     <MenuItem onClick={logoutClickHandler}> Logout</MenuItem>
                   </Menu>
